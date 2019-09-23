3ece49ac3c7a9bdecb19c2f2458ccac7e482e2dc
Russell <russell@union-pos.com>
2019-09-23 12:19:15 -0500

    Added policy for km

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.14.1"
+  source     = "git::https://github.com/UnionPOS/terraform-null-label.git?ref=tags/0.16.0"
   namespace  = var.namespace
   stage      = var.stage
   name       = var.name
@@ -8,11 +8,36 @@ module "label" {
   tags       = var.tags
 }
 
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "default" {
+  statement {
+    sid       = "Enable IAM User Permissions"
+    effect    = "Allow"
+    actions   = ["kms:*"]
+    resources = ["*"]
+
+    principals {
+      type = "AWS"
+
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
+      ]
+    }
+  }
+}
+
+module "policy" {
+  source           = "git::https://github.com/UnionPOS/terraform-aws-iam-policy-document-aggregator.git?ref=tf12"
+  source_documents = ["${data.aws_iam_policy_document.default.json}", "${var.policy}"]
+}
+
 resource "aws_kms_key" "default" {
   deletion_window_in_days = var.deletion_window_in_days
   enable_key_rotation     = var.enable_key_rotation
   tags                    = module.label.tags
   description             = var.description
+  policy                  = module.policy.result_document
 }
 
 resource "aws_kms_alias" "default" {
